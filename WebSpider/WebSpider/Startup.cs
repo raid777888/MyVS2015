2ce017ecd770d6fbabac1226461393b9ad3cc8f4
raid777888 <279608040@qq.com>
2016-10-09 17:30:12 +0800

    Revert "Revert "NEW""
    This reverts commit dc0c02298a6856c9c9bed3c1eadca8ec85865e1f.

---
+++ WebSpider/WebSpider/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSpider.Startup))]
+namespace WebSpider
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
